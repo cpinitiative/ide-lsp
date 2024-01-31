d0ac61d3fc0d6492a6659e73c469361762d38231
Daniel Zhu <danielfangzhu@gmail.com>
2024-01-31 19:04:35 +0000

    add connection limit

--- src/json-server-launcher.ts
+++ src/json-server-launcher.ts
@@ -6,14 +6,22 @@ import * as rpc from "vscode-ws-jsonrpc/cjs";
 import * as server from "vscode-ws-jsonrpc/cjs/server";
 import * as lsp from "vscode-languageserver";
 import { Message } from "vscode-languageserver";
-
+let connections = 0;
+const MAX_CONNECTIONS = 1;
 export function launch(socket: rpc.IWebSocket) {
   const reader = new rpc.WebSocketMessageReader(socket);
   const writer = new rpc.WebSocketMessageWriter(socket);
-
+  if (connections >= MAX_CONNECTIONS) {
+    writer.write({ jsonrpc: '2.0', method: 'reject' } as rpc.NotificationMessage);
+    return;
+  }
+  console.log('connections: ', ++connections);
+  writer.write({ jsonrpc: '2.0', method: 'start' } as rpc.NotificationMessage);
   // start the language server as an external process
-  const socketConnection = server.createConnection(reader, writer, () =>
-    socket.dispose()
+  const socketConnection = server.createConnection(reader, writer, () => {
+      --connections;
+      socket.dispose();
+    }
   );
   const serverConnection = server.createServerProcess("CPP", "clangd-12");
   if (serverConnection) {
