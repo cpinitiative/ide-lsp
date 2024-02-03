f11451cdc0b49a0d4c20065c2a8cdd0cec6d1207
Daniel Zhu <danielfangzhu@gmail.com>
2024-02-03 22:23:39 +0000

    add ping logic

--- src/json-server-launcher.ts
+++ src/json-server-launcher.ts
@@ -7,10 +7,11 @@ import * as server from "vscode-ws-jsonrpc/cjs/server";
 import * as lsp from "vscode-languageserver";
 import { Message } from "vscode-languageserver";
 let connections = 0;
-const MAX_CONNECTIONS = 1;
+const MAX_CONNECTIONS = 10;
 export function launch(socket: rpc.IWebSocket) {
   const reader = new rpc.WebSocketMessageReader(socket);
   const writer = new rpc.WebSocketMessageWriter(socket);
+  let lastDate = new Date();
   if (connections >= MAX_CONNECTIONS) {
     writer.write({ jsonrpc: '2.0', method: 'reject' } as rpc.NotificationMessage);
     return;
@@ -24,12 +25,24 @@ export function launch(socket: rpc.IWebSocket) {
     }
   );
   const serverConnection = server.createServerProcess("CPP", "clangd-12");
+  setInterval(() => {
+    if (new Date().getTime() - lastDate.getTime() > 30000) {
+      console.log('connection timed out!');
+      serverConnection?.dispose();
+    }
+  }, 10000)
   if (serverConnection) {
     server.forward(socketConnection, serverConnection, (message) => {
+      console.log(message);
       if (Message.isRequest(message)) {
         if (message.method === lsp.InitializeRequest.type.method) {
           const initializeParams = message.params as lsp.InitializeParams;
           initializeParams.processId = process.pid;
+        }
+      }
+      else if (Message.isNotification(message)) {
+        if (message.method === 'ping') {
+          lastDate = new Date();
         }
       }
       return message;
