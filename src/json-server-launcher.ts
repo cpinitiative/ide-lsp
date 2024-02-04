b592985e2bcca26b783f159fd3740b885fb0fdaf
Daniel Zhu <danielfangzhu@gmail.com>
2024-02-04 05:39:03 +0000

    minor updates

--- src/json-server-launcher.ts
+++ src/json-server-launcher.ts
@@ -4,7 +4,6 @@
  * ------------------------------------------------------------------------------------------ */
 import * as rpc from "vscode-ws-jsonrpc/cjs";
 import * as server from "vscode-ws-jsonrpc/cjs/server";
-import * as lsp from "vscode-languageserver";
 import { Message } from "vscode-languageserver";
 let connections = 0;
 const MAX_CONNECTIONS = 40;
@@ -17,37 +16,33 @@ export function launch(socket: rpc.IWebSocket) {
     console.log(`rejected connection at ${new Date()} (connections: ${connections})`)
     return;
   }
-  console.log(`at ${new Date()} - ${++connections} connections`);
+  console.log(`new connection at ${new Date()} - ${++connections} connections`);
   writer.write({ jsonrpc: '2.0', method: 'start' } as rpc.NotificationMessage);
-  // start the language server as an external process
-  const socketConnection = server.createConnection(reader, writer, () => {
-      --connections;
-      socket.dispose();
-    }
-  );
-  const serverConnection = server.createServerProcess("CPP", "clangd-12");
-  const id = setInterval(() => {
+  let socketConnection: server.IConnection | undefined;
+  let serverConnection: server.IConnection | undefined;
+  let pingId = setInterval(() => {
     if (new Date().getTime() - lastDate.getTime() > 30000) {
       console.log('connection timed out!');
       serverConnection?.dispose();
-      socketConnection.dispose();
-      clearInterval(id);
+      socketConnection?.dispose();
+      clearInterval(pingId);
     }
-  }, 10000)
-  if (serverConnection) {
-    server.forward(socketConnection, serverConnection, (message) => {
-      if (Message.isRequest(message)) {
-        if (message.method === lsp.InitializeRequest.type.method) {
-          const initializeParams = message.params as lsp.InitializeParams;
-          initializeParams.processId = process.pid;
-        }
-      }
-      else if (Message.isNotification(message)) {
-        if (message.method === 'ping') {
-          lastDate = new Date();
-        }
-      }
-      return message;
-    });
-  }
+  }, 10000);
+  // start the language server as an external process
+  socketConnection = server.createConnection(reader, writer, () => {
+      console.log(`closed connection at ${new Date()} - ${--connections} connections`);
+      socket.dispose();
+      clearInterval(pingId);
+    }
+  );
+  serverConnection = server.createServerProcess("CPP", "clangd-12");
+  reader.listen(message => {
+    if (Message.isNotification(message) && message.method === 'ping') {
+      lastDate = new Date();
+    }
+    else serverConnection?.writer.write(message);
+  });
+  serverConnection?.reader.listen(message => {
+    writer.write(message);
+  })
 }
