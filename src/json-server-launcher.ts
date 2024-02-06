035aebd39dfc3ed8f6109c4e29b110ea4c99e441
Daniel Zhu <danielfangzhu@gmail.com>
2024-02-06 05:43:54 +0000

    fix issues

--- src/json-server-launcher.ts
+++ src/json-server-launcher.ts
@@ -12,6 +12,7 @@ const MAX_CONNECTIONS = 40;
 function forward(
   from: server.IConnection,
   to: server.IConnection,
+  id: number,
   map?: (message: Message) => Message | undefined
 ) {
   from.reader.listen(message => {
@@ -22,10 +23,17 @@ function forward(
     const res = map ? map(message) : message;
     if (res) from.writer.write(res);
   });
-  from.onClose(() => to.dispose());
-  to.onClose(() => from.dispose());
+  from.onClose(() => {
+    console.log('disposing server ' + id);
+    to.dispose();
+  });
+  to.onClose(() => {
+    console.log('disposing client ' + id);
+    from.dispose();
+  });
 }
 export function launch(socket: rpc.IWebSocket) {
+  const id = Math.floor(Math.random() * 1000);
   const reader = new rpc.WebSocketMessageReader(socket);
   const writer = new rpc.WebSocketMessageWriter(socket);
   let lastDate = new Date();
@@ -39,28 +47,30 @@ export function launch(socket: rpc.IWebSocket) {
     );
     return;
   }
-  console.log(`new connection at ${new Date()} - ${++connections} connections`);
+  console.log(
+    `new connection at ${new Date()} - ${++connections} connections (socket id: ${id})`
+  );
   writer.write({ jsonrpc: '2.0', method: 'start' } as rpc.NotificationMessage);
   let socketConnection: server.IConnection | undefined;
   let serverConnection: server.IConnection | undefined;
   const pingId = setInterval(() => {
     if (new Date().getTime() - lastDate.getTime() > 30000) {
-      console.log('connection timed out!');
-      socketConnection?.dispose();
+      console.log(`connection #${id} timed out!`);
+      serverConnection?.dispose();
     }
   }, 10000);
 
   // start servers
   socketConnection = server.createConnection(reader, writer, () => {
     console.log(
-      `closed connection at ${new Date()} - ${--connections} connections`
+      `closed connection at ${new Date()} - ${--connections} connections (socket #${id})`
     );
     clearInterval(pingId);
   });
 
   serverConnection = server.createServerProcess('CPP', 'clangd-12');
   if (!serverConnection) return;
-  forward(socketConnection, serverConnection, message => {
+  forward(socketConnection, serverConnection, id, message => {
     if (Message.isNotification(message) && message.method === 'ping') {
       lastDate = new Date();
       return undefined;
